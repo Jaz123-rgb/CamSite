7c8baeafe0d885e6fc9efaed1b32ce2f693a9b0b
jaz <70194414+Jaz123-rgb@users.noreply.github.com>
2022-12-02 17:48:32 -0600

    Pagina avances navabar
    hardshit

--- src/componets/navabar.js
+++ src/componets/navabar.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button'
+import './navbar.css'
 function Navbar() {
   const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
+  
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
+
+  const showButton = ()=>{
+        if (window.innerWidth <= 960){
+            setButton(false); 
+             }else{
+                setButton(true);
+            }
+        }
+
+window.addEventListener('resize', showButton)
+
   return (
     <>
-    <nav className='NavBar'>
-        <div className='Navbar-container'> 
+    <nav className='navbar'>
+        <div className='navbar-container'> 
             <Link to="/" className='navbar-logo'>
-                CAMSEG <i className='fab fa-typo3'/>
+                CAMDESEG <i className='fab fa-typo3'/>
             </Link>
             <div className='menu-icon' onClick={handleClick} > 
               <i className={click ? 'fas fa-times': 'fas fa-bars'}/>
@@ -17,32 +32,32 @@ function Navbar() {
 
             <ul className={click? 'nav-menu  active' : 'nav-menu'}>
                 {/*  Home redirect */}
-                <li className='nav-time'>
+                <li className='nav-item'>
                     <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                     home
                     </Link>
                 </li>
                 {/*  Servicsio redirect */}
-                <li className='nav-time'>
+                <li className='nav-item'>
                     <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                     Servcios
                     </Link>
                 </li>
                 {/*  Home redirect */}
-                <li className='nav-time'>
+                <li className='nav-item'>
                     <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                     Propductos
                     </Link>
                 </li>
 
                 {/*  Home redirect */}
-                <li className='nav-time'>
+                <li className='nav-item'>
                     <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
                     Ingresar
                     </Link>
                 </li>
             </ul>
-
+            {button && <Button buttonStyle='btn--outline'>Ingresar</Button>}
         </div>
     </nav>
     </>
