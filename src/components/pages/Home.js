b0649efa6710f12c77f35503960fbb17845eb980
jaz <70194414+Jaz123-rgb@users.noreply.github.com>
2022-12-05 18:03:09 -0600

    upate con errores
    be careful with the new add

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,13 +3,16 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import Example from '../HomeMain/Careles'
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Cards />
+      <Cards/>
+      <Example/>
       <Footer />
+      
     </>
   );
 }
